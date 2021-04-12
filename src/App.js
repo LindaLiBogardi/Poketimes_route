c33c5883e0edb37c53b0e1113131db0f89a1f242
Linda Li Bogardi <linli781@gmail.com>
2021-04-12 22:36:15 +0200

    fixed exact routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Navbar from './compoments/Navbar'
 import {BrowserRouter, Route} from 'react-router-dom'
 import Home from './compoments/Home'
+import About from './compoments/About'
+import Contact from './compoments/Contact'
 
 
 class App extends Component {
@@ -10,7 +12,9 @@ class App extends Component {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route path='/' component={Home} />
+        <Route exact path='/home' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
       </div>
     </BrowserRouter>
 
